91f20640898d3aa66e5694a6cb09ebb53c69b770
rafsaf <rafal.safin12@gmail.com>
2020-09-21 16:15:57 +0200

    Updates

---
+++ precache-manifest.03a04b2e3e7ae2e0ab283be818c1f6bc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16c8dca98dd3997106670d5272588519",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "488282f13935fd9f692f",
+    "url": "/portfolio/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "f5ae711475f8ece816ea",
+    "url": "/portfolio/static/css/main.2483e90b.chunk.css"
+  },
+  {
+    "revision": "488282f13935fd9f692f",
+    "url": "/portfolio/static/js/2.647955ca.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/portfolio/static/js/2.647955ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5ae711475f8ece816ea",
+    "url": "/portfolio/static/js/main.87645459.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "d419b7664118150ae0438d9c3bae21c8",
+    "url": "/portfolio/static/media/table.d419b766.jpg"
+  }
+]);
